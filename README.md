c34d39683aa28cf64583ecc683db6d28675b20d4
captjay98 <captjay98@gmail.com>
2024-01-17 08:28:48 +0100

    Running on localhost

---
+++ README.md
@@ -0,0 +1,55 @@
+You can use the script[setup.sh] to do this automagically and just Press Enter when you're done to stop the servers.
+
+I recommend using a local DB.
+
+# Project Setup Instructions
+
+## Clone the repository
+
+git clone https://github.com/captjay98/todopro
+
+## CD into the directory
+
+cd todopro
+
+## Copy the example environment file
+
+cp exampleenv .env
+
+THE DB IS REALLY SLOW, If you can edit the env file and use a db that's available locally, It'll be much better.
+
+## Install Backend dependencies
+
+composer install
+
+## Clear config Cache and Cache
+
+php artisan cache:clear
+php artisan config:clear
+php artisan config:cache
+
+## Run database migrations
+
+php artisan migrate
+
+## Seed the database
+
+php artisan db:seed
+
+## Install frontend dependencies
+
+yarn install or npm install
+
+## Start the Frontend Dev server in the background
+
+Better to run it in the foreground and run the backend in another tab
+
+npm run dev &
+
+## Go back to the project root directory
+
+cd ..
+
+## Start the Backend Dev server
+
+php artisan serve &
--- setup.sh
+++ setup.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 set -e
 
+# kill a process by name
+kill_process() {
+  local process_name="$1"
+  local pid
+
+  pid=$(pgrep -f "$process_name")
+
+  if [[ -n "$pid" ]]; then
+    echo "Killing $process_name (PID: $pid)..."
+    kill "$pid"
+    echo "Successfully killed $process_name."
+  else
+    echo "$process_name is not running."
+  fi
+}
+
+# Stop the servers
+stop_servers() {
+  echo "Killing all services..."
+  kil_process "php artisan serve"
+  kill_process "npm run dev"
+}
+
 # Clone the repository
 # git clone https://github.com/captjay98/todopro
 # cd todopro
@@ -10,6 +33,11 @@ cp exampleenv .env
 
 # Install dependencies
 composer install
+
+# Clear config Cache and Cache
+php artisan cache:clear
+php artisan config:clear
+php artisan config:cache
 
 # Run database migrations
 php artisan migrate
@@ -31,3 +59,9 @@ cd ..
 php artisan serve &
 
 echo "Setup complete. Servers running in the background."
+
+# Kill services on script exit
+trap kill_services EXIT
+
+# Wait for user input to exit the script
+read -rp "Press Enter to stop the services and exit..."
