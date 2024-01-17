21b1bf2071103c1ccb2c5a680f0f083464b6fcfd
captjay98 <captjay98@gmail.com>
2024-01-17 07:20:22 +0100

    layout files

---
+++ todopro-frontend/src/layouts/AuthenticatedLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavBar from '@/components/partials/NavBarComponent.vue'
+</script>
+<template>
+  <div class="pt-10 m-auto w-full h-full md:pt-2">
+    <NavBar />
+    <div class="overflow-y-auto px-3 w-full h-full">
+      <slot />
+    </div>
+  </div>
+</template>
