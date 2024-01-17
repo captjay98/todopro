f4adf5bcf7a7d60870abcba6211894d3af87ae38
captjay98 <captjay98@gmail.com>
2024-01-17 07:21:48 +0100

    App file

---
+++ todopro-frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div class="w-screen h-screen bg-black overflow-clip">
+        <div class="w-full h-full bg-blue-900/20">
+            <router-view />
+        </div>
+    </div>
+</template>
