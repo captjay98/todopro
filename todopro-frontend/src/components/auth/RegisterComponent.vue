fd86f5638ccfc99f6c5fd350e628f6206bed985f
captjay98 <captjay98@gmail.com>
2024-01-21 19:35:52 +0100

    Refactored Login and Registration, moved the logic to the userStore updated login and register component to reflect the change

--- todopro-frontend/src/components/auth/RegisterComponent.vue
+++ todopro-frontend/src/components/auth/RegisterComponent.vue
@@ -1,132 +1,64 @@
 <script setup>
 import Button from '@/components/partials/ButtonComponent.vue'
-import { useRouter } from 'vue-router'
 import { ref } from 'vue'
-import axiosInstance from '@/middlewares/axiosInstance.js'
-import { useToast } from 'vue-toastification'
-import { useUserStore } from '@/stores/userStore.js'
-const router = useRouter()
-const toast = useToast()
-
-const loading = ref(false)
-const message = ref('{}')
-const form = ref({
-  name: '',
-  email: '',
-  password: '',
-  password_confirmation: ''
-})
+import { useUserStore } from '@/stores/userStore'
 
 const userStore = useUserStore()
+const loading = ref(false)
+const form = ref({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+})
 
-/**
- * Registers a new user by sending form data to the /register endpoint.
- * It handles the success and error cases by showing appropriate toast messages.
- * On successful registration, it stores the token, sets the authorization header,
- * and navigates to the 'home' route.
- */
+// Asynchronously registers a user using the provided form, loading, and toast.
 const register = async () => {
-  message.value = {}
-  loading.value = true
-  toast('Signing you up...', { timeout: false })
-
-  try {
-    // Obtain CSRF token
-    await axiosInstance.get('sanctum/csrf-cookie')
-    const response = await axiosInstance.post('register', form.value)
-    localStorage.setItem('token', response.data.token)
-    axiosInstance.defaults.headers.common['Authorization'] =
-      `Bearer ${localStorage.getItem('token')}`
-    toast.clear()
-    toast.success('Welcome to TodoPro!')
-
-    const user = response.data.user
-    userStore.setUser(user)
-    router.push('/dashboard')
-  } catch (error) {
-    toast.clear()
-    if (error.response) {
-      if (error.response.status === 422) {
-        // Validation errors
-        for (const fieldErrors of Object.values(error.response.data.errors)) {
-          fieldErrors.forEach((errorMessage) => {
-            toast.error(errorMessage)
-          })
-        }
-      } else if (error.response.data && error.response.data.message) {
-        toast.error(error.response.data.message)
-      }
-    } else {
-      toast.error('There was a network error. Please try again later.')
-    }
-  } finally {
-    loading.value = false
-    form.value.password = ''
-    form.value.password_confirmation = ''
-  }
+    await userStore.registerUser(form, loading)
 }
 </script>
 <template>
-  <div class="flex justify-end w-full h-full text-2xl tracking-normal text-center text-slate-200">
-    <div class="m-auto w-[20rem] h-[30rem]">
-      <h1 class="pb-10 text-2xl font-semibold tracking-wider text-center text-slate-300">
-        CREATE ACCOUNT
-      </h1>
-      <form @submit.prevent="register" class="flex flex-col gap-4">
-        <div class="flex flex-col">
-          <label class="px-1 text-left text-[1.0rem]" for="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            v-model="form.name"
-            class="py-1 px-2 rounded-lg focus:border-blue-700 focus:ring-0 focus:outline-none active:bg-blue-500 text-[1.2rem] bg-slate-600/90 h-[2.3rem] focus:border-[0.2rem]"
-            required
-          />
-        </div>
-        <div class="flex flex-col">
-          <label class="px-1 text-left text-[1.0rem]" for="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            v-model="form.email"
-            class="py-1 px-2 rounded-lg focus:border-blue-700 focus:ring-0 focus:outline-none active:bg-blue-500 text-[1.2rem] bg-slate-600/90 h-[2.3rem] focus:border-[0.2rem]"
-            autocomplete
-            required
-          />
-        </div>
-        <div class="flex flex-col">
-          <label class="px-1 text-left text-[1.0rem]" for="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            v-model="form.password"
-            class="py-1 px-2 rounded-lg focus:border-blue-700 focus:ring-0 focus:outline-none active:bg-blue-500 text-[1.2rem] bg-slate-600/90 h-[2.3rem] focus:border-[0.2rem]"
-            required
-          />
-        </div>
-        <div class="flex flex-col">
-          <label class="px-1 text-left text-[1.0rem]" for="password_confirmation"
-            >Confirm Password</label
-          >
-          <input
-            type="password"
-            id="password_confirmation"
-            v-model="form.password_confirmation"
-            class="py-1 px-2 rounded-lg focus:border-blue-700 focus:ring-0 focus:outline-none active:bg-blue-500 text-[1.2rem] bg-slate-600/90 h-[2.3rem] focus:border-[0.2rem]"
-            required
-          />
-        </div>
+    <div class="flex justify-end w-full h-full text-2xl tracking-normal text-center text-slate-200">
+        <div class="m-auto w-[20rem] h-[30rem]">
+            <h1 class="pb-10 text-2xl font-semibold tracking-wider text-center text-slate-300">
+                CREATE ACCOUNT
+            </h1>
+            <form @submit.prevent="register" class="flex flex-col gap-4">
+                <div class="flex flex-col">
+                    <label class="px-1 text-left text-[1.0rem]" for="name">Name</label>
+                    <input type="text" id="name" v-model="form.name"
+                        class="py-1 px-2 rounded-lg focus:border-blue-700 focus:ring-0 focus:outline-none active:bg-blue-500 text-[1.2rem] bg-slate-600/90 h-[2.3rem] focus:border-[0.2rem]"
+                        required />
+                </div>
+                <div class="flex flex-col">
+                    <label class="px-1 text-left text-[1.0rem]" for="email">Email</label>
+                    <input type="email" id="email" v-model="form.email"
+                        class="py-1 px-2 rounded-lg focus:border-blue-700 focus:ring-0 focus:outline-none active:bg-blue-500 text-[1.2rem] bg-slate-600/90 h-[2.3rem] focus:border-[0.2rem]"
+                        autocomplete required />
+                </div>
+                <div class="flex flex-col">
+                    <label class="px-1 text-left text-[1.0rem]" for="password">Password</label>
+                    <input type="password" id="password" v-model="form.password"
+                        class="py-1 px-2 rounded-lg focus:border-blue-700 focus:ring-0 focus:outline-none active:bg-blue-500 text-[1.2rem] bg-slate-600/90 h-[2.3rem] focus:border-[0.2rem]"
+                        required />
+                </div>
+                <div class="flex flex-col">
+                    <label class="px-1 text-left text-[1.0rem]" for="password_confirmation">Confirm Password</label>
+                    <input type="password" id="password_confirmation" v-model="form.password_confirmation"
+                        class="py-1 px-2 rounded-lg focus:border-blue-700 focus:ring-0 focus:outline-none active:bg-blue-500 text-[1.2rem] bg-slate-600/90 h-[2.3rem] focus:border-[0.2rem]"
+                        required />
+                </div>
 
-        <div class="mt-4 w-full">
-          <Button :disabled="loading" class="w-full disabled:bg-blue-900/50">SIGN UP </Button>
+                <div class="mt-4 w-full">
+                    <Button :disabled="loading" class="w-full disabled:bg-blue-900/50">SIGN UP </Button>
+                </div>
+                <div>
+                    <p class="text-sm">
+                        Already have an account?
+                        <RouterLink to="/login"><span class="text-blue-500">Login</span></RouterLink>
+                    </p>
+                </div>
+            </form>
         </div>
-        <div>
-          <p class="text-sm">
-            Already have an account?
-            <RouterLink to="/login"><span class="text-blue-500">Login</span></RouterLink>
-          </p>
-        </div>
-      </form>
     </div>
-  </div>
 </template>
--- todopro-frontend/src/stores/userStore.js
+++ todopro-frontend/src/stores/userStore.js
@@ -1,77 +1,137 @@
-import { defineStore } from 'pinia'
+import { ref } from 'vue'
 import axiosInstance from '@/middlewares/axiosInstance.js'
-
 import { useToast } from 'vue-toastification'
 import router from '@/router'
 
-const toast = useToast()
+export function useUserStore() {
+  const user = ref(null)
+  const toast = useToast()
 
-export const useUserStore = defineStore('user', {
   /**
-   * Returns an object representing the initial state of the component.
-   *
-   * @return {Object} - The initial state object.
+   * Asynchronously sets the user by making a GET request to '/api/user'.
+   * @param {Object} user - The user object to set, passed after login or registration.
+   * @return {Promise<Object>} The user object fetched from the API.
+   * @throws {Error} If there is an error during the request.
    */
-  state: () => ({
-    user: null
-  }),
-  actions: {
-    /**
-     * Asynchronously sets the user by making a GET request to '/api/user'.
-     * @param {Object} user - The user object to set, passed after login or registration.
-     * @return {Promise<Object>} The user object fetched from the API.
-     * @throws {Error} If there is an error during the request.
-     */
-    async setUser(user) {
-      if (user) {
-        this.user = user
-        router.push('/dashboard')
-      } else {
-        try {
-          const response = await axiosInstance.get('/api/user')
-          this.user = response.data.data
-          return this.user
-        } catch (error) {
-          if (error.response.data) {
-            if (router.currentRoute.value.path === '/') {
-              return
-            } else if (error.response.data.message) {
-              toast.error(error.response.data.message)
-            } else {
-              toast.error(`Coudn't Set User. ${error.response.statusText}`)
-            }
-          } else {
-            toast.error('There was a network error. Please try again later.')
-          }
-          this.user = null
-        }
-      }
-    },
-    /**
-     * Logout function.
-     *
-     * @return {Promise} Promise that resolves when the logout is successful.
-     */
-    async logout() {
-      try {
-        const response = await axiosInstance.post('logout')
-        if (response.data.success === true) {
-          localStorage.removeItem('token')
-          this.user = null
-          router.go('/')
-          toast.success('You have been Logged Out!')
-        }
-      } catch (error) {
-        if (error.response.data) {
-          if (error.response.data.message) {
-            toast.error(error.response.data.message)
-          } else {
-            toast.error(`Coudn't fetch Todos. ${error.response.statusText}`)
-          }
-        } else {
-          toast.error('There was a network error. Please try again later.')
-        }
-      }
+
+  const setUser = async () => {
+    try {
+      const response = await axiosInstance.get('/api/user')
+      user.value = response.data.data
+      return user.value
+    } catch (error) {
+      handleErrors(error)
+      user.value = null
     }
   }
-})
+
+  /**
+   * Asynchronously Attempts logs in the user using the provided form data.
+   * It obtains a CSRF token, sends the login credentials to /login endpoint,
+   * and then handles success and error responses. On success, it stores the token,
+   * sets the authorization header, updates user state, and redirects to the 'home' route.
+   *  On error, it shows a relevant error message using toast notifications.
+   * @param {object} form - The user login form data.
+   * @param {object} loading - The loading indicator state object.
+   * @param {object} toast - The toast notification object.
+   */
+  const loginUser = async (form, loading) => {
+    loading.value = true
+    toast.info('Signing in...', { timeout: 1000 })
+    try {
+      await axiosInstance.get('/sanctum/csrf-cookie')
+      const response = await axiosInstance.post('login', form.value)
+      handleLoginResponse(response)
+      toast.success('Welcome back!')
+      router.push('/dashboard')
+    } catch (error) {
+      handleErrors(error)
+    } finally {
+      clearFormPassword(form)
+      loading.value = false
+    }
+  }
+
+  /**
+   * Asynchronously Attempts to Register a User using the provided form data.
+   * It obtains a CSRF token, sends the data to /register endpoint,
+   * and then handles success and error responses. On success, it stores the token,
+   * sets the authorization header, updates user state, and redirects to the 'home' route.
+   * On error, it shows a relevant error message using toast notifications.
+   * @param {object} form - The user Registration form data.
+   * @param {object} loading - The loading indicator state object.
+   * @param {object} toast - The toast notification object.
+   */
+  const registerUser = async (form, loading) => {
+    loading.value = true
+    toast.info('Signing you up!', { timeout: 1000 })
+    try {
+      await axiosInstance.get('sanctum/csrf-cookie')
+      const response = await axiosInstance.post('register', form.value)
+      handleLoginResponse(response)
+      toast.success('Welcome to TodoPro!')
+      router.push('/dashboard')
+    } catch (error) {
+      handleErrors(error)
+    } finally {
+      clearFormPassword(form)
+      loading.value = false
+    }
+  }
+
+  const logout = async () => {
+    toast.info('logging you out')
+    try {
+      const response = await axiosInstance.post('logout')
+      if (response.data.success === true) {
+        localStorage.removeItem('token')
+        user.value = null
+        // router.push();
+        location.reload()
+        toast.success('You have been logged out!')
+      }
+    } catch (error) {
+      handleErrors(error)
+    }
+  }
+
+  // Helper function to handle login response
+  const handleLoginResponse = (response) => {
+    localStorage.setItem('token', response.data.token)
+    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`
+    user.value = response.data.user
+  }
+
+  // Helper function to clear the form password fields
+  const clearFormPassword = (form) => {
+    form.value.password = ''
+    form.value.password_confirmation = ''
+  }
+
+  // Helper function to handle errors
+  const handleErrors = (error) => {
+    if (error.response) {
+      if (error.response.status === 422) {
+        for (const fieldErrors of Object.values(error.response.data.errors)) {
+          fieldErrors.forEach((errorMessage) => {
+            toast.error(errorMessage)
+          })
+        }
+      } else if (error.response.data && error.response.data.message) {
+        toast.error(error.response.data.message)
+      } else if (error.response.data && error.response.data.error) {
+        toast.error(error.response.data.error)
+      }
+    } else {
+      toast.error('Something went wrong')
+    }
+  }
+
+  return {
+    user,
+    setUser,
+    loginUser,
+    registerUser,
+    logout
+  }
+}
